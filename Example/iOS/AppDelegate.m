9bef1a5367c3b458a7ff0e457b16db47d9d03272
Joel Arvidsson <joel@oblador.se>
2015-08-28 14:51:59 +0200

    Bumped example to RN 0.11-rc.

--- Example/iOS/AppDelegate.m
+++ Example/iOS/AppDelegate.m
@@ -47,6 +47,7 @@
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"Example"
+                                               initialProperties:nil
                                                    launchOptions:launchOptions];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
