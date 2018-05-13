d3c9fd76cd6fbf662e4740ff01db7bcf64e72516
Joel Arvidsson <joel@oblador.se>
2018-05-13 21:51:05 +0200

    Remove unused import

--- src/Zoomable.ios.js
+++ src/Zoomable.ios.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { ScrollView, Image, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
