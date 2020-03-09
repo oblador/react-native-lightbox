b28b7b59d31184b4a7797d77f413b537b9e55cf9
Nico Hernandez <nicoo.her@hotmail.com>
2020-03-09 15:27:22 -0300

    Update types.d.ts

--- types.d.ts
+++ types.d.ts
@@ -2,17 +2,17 @@ declare module 'react-native-lightbox' {
   import { Animated } from 'react-native'
 
   interface LightboxProps {
-    activeProps: any
-    renderHeader: () => void
-    renderContent: () => void
-    underlayColor: string
-    backgroundColor: string
-    didOpen: () => void
-    onOpen: () => void
-    willClose: () => void
-    onClose: () => void
-    springConfig: Animated.SpringAnimationConfig['friction' | 'tension']
-    swipeToDismiss: boolean
+    activeProps?: any
+    renderHeader?: () => void
+    renderContent?: () => void
+    underlayColor?: string
+    backgroundColor?: string
+    didOpen?: () => void
+    onOpen?: () => void
+    willClose?: () => void
+    onClose?: () => void
+    springConfig?: Animated.SpringAnimationConfig['friction' | 'tension']
+    swipeToDismiss?: boolean
   }
 
   const Lightbox: React.FC<LightboxProps>
