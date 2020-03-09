ae4373858120e0f03d89076d610aff59222672bf
Nico Hernandez <nicoo.her@hotmail.com>
2020-03-09 15:21:23 -0300

    Add typescript support.

---
+++ types.d.ts
@@ -0,0 +1,21 @@
+declare module 'react-native-lightbox' {
+  import { Animated } from 'react-native'
+
+  interface LightboxProps {
+    activeProps: any
+    renderHeader: () => void
+    renderContent: () => void
+    underlayColor: string
+    backgroundColor: string
+    didOpen: () => void
+    onOpen: () => void
+    willClose: () => void
+    onClose: () => void
+    springConfig: Animated.SpringAnimationConfig['friction' | 'tension']
+    swipeToDismiss: boolean
+  }
+
+  const Lightbox: React.FC<LightboxProps>
+
+  export default Lightbox
+}
