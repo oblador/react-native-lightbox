ecb5b588f7da0d57a5dca1132ec805d7ed22c700
Ricardo Vaz Correa <ricardovcorrea@live.com>
2020-05-08 23:10:38 -0300

    New example with updated react-native

--- Example/app.js
+++ Example/app.js
@@ -8,6 +8,7 @@ import {
   Text,
   TouchableOpacity,
   View,
+  SafeAreaView
 } from 'react-native';
 
 import Lightbox from 'react-native-lightbox';
@@ -30,45 +31,47 @@ const renderCarousel = () => (
 )
 
 export default () => (
-  <ScrollView style={styles.container}>
-    <View style={styles.text}><Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Text></View>
-    <Lightbox underlayColor="white">
-      <Image
-        style={styles.contain}
-        resizeMode="contain"
-        source={{ uri: 'https://www.yayomg.com/wp-content/uploads/2014/04/yayomg-pig-wearing-party-hat.jpg' }}
-      />
-    </Lightbox>
-    <View style={styles.text}><Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Text></View>
-    <Lightbox springConfig={{tension: 15, friction: 7}} swipeToDismiss={false} renderContent={renderCarousel}>
-      <Image
-        style={styles.carousel}
-        resizeMode="contain"
-        source={{ uri: 'http://cdn.lolwot.com/wp-content/uploads/2015/07/20-pictures-of-animals-in-hats-to-brighten-up-your-day-1.jpg' }}
-      />
-    </Lightbox>
-    <View style={styles.text}><Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Text></View>
-    <Lightbox
-      renderHeader={close => (
-        <TouchableOpacity onPress={close}>
-          <Text style={styles.closeButton}>Close</Text>
-        </TouchableOpacity>
-      )}>
-      <View style={styles.customHeaderBox}>
-        <Text>I have a custom header</Text>
-      </View>
-    </Lightbox>
-    <View style={styles.text}><Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Text></View>
-    <View style={styles.row}>
-      <Lightbox style={styles.col}>
-        <View style={[styles.square, styles.squareFirst]}><Text style={styles.squareText}>I'm a square</Text></View>
-      </Lightbox>
-      <Lightbox style={styles.col}>
-        <View style={[styles.square, styles.squareSecond]}><Text style={styles.squareText}>I'm a square</Text></View>
-      </Lightbox>
-    </View>
-    <View style={styles.text}><Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Text></View>
-  </ScrollView>
+    <SafeAreaView>
+      <ScrollView style={styles.container}>
+        <View style={styles.text}><Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Text></View>
+        <Lightbox underlayColor="white">
+          <Image
+            style={styles.contain}
+            resizeMode="contain"
+            source={{ uri: 'https://www.yayomg.com/wp-content/uploads/2014/04/yayomg-pig-wearing-party-hat.jpg' }}
+          />
+        </Lightbox>
+        <View style={styles.text}><Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Text></View>
+        <Lightbox springConfig={{tension: 15, friction: 7}} swipeToDismiss={false} renderContent={renderCarousel}>
+          <Image
+            style={styles.carousel}
+            resizeMode="contain"
+            source={{ uri: 'http://cdn.lolwot.com/wp-content/uploads/2015/07/20-pictures-of-animals-in-hats-to-brighten-up-your-day-1.jpg' }}
+          />
+        </Lightbox>
+        <View style={styles.text}><Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Text></View>
+        <Lightbox
+          renderHeader={close => (
+            <TouchableOpacity onPress={close}>
+              <Text style={styles.closeButton}>Close</Text>
+            </TouchableOpacity>
+          )}>
+          <View style={styles.customHeaderBox}>
+            <Text>I have a custom header</Text>
+          </View>
+        </Lightbox>
+        <View style={styles.text}><Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Text></View>
+        <View style={styles.row}>
+          <Lightbox style={styles.col}>
+            <View style={[styles.square, styles.squareFirst]}><Text style={styles.squareText}>I'm a square</Text></View>
+          </Lightbox>
+          <Lightbox style={styles.col}>
+            <View style={[styles.square, styles.squareSecond]}><Text style={styles.squareText}>I'm a square</Text></View>
+          </Lightbox>
+        </View>
+        <View style={styles.text}><Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Text></View>
+      </ScrollView>
+    </SafeAreaView>
 )
 
 const styles = StyleSheet.create({
